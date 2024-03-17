5eac61c0b577c70e9b869099bb1a5f326bca1dce
paulobravo <p_m_c_bravo@yahoo.com>
2024-03-17 16:44:35 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
+import portfolio from './service/portf';
 
 function App() {
+
+  console.log(portfolio[0].architect)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{portfolio[0].architect}</h1>
+      <p>{portfolio[0].local}</p>
+      <p>{portfolio[0].software}</p>
     </div>
   );
 }
